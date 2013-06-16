09f71fb5028486b64d5cdc38d5b5b562b4e1f4bc
Ruslana Kiverskaya <ruslana.kiverskaya@gmail.com>
2013-06-16 20:06:30 +0400

    check the image size

--- lib/image_hopper.rb
+++ lib/image_hopper.rb
@@ -10,6 +10,7 @@ module ImageHopper
     attr_accessor :site_url, :dir_path, :images
     
     USER_AGENT = "Google Chrome 26.0.1410.43"
+    MIN_SIZE = 4000
     
     def initialize(site_url, dir_path)
       @site_url = site_url
@@ -23,22 +24,27 @@ module ImageHopper
       content = open(parse).read
       
       content.scan(/['"]+([^'"]+\.(?:#{Image::IMAGE_EXTENSIONS.join('|')}))[^'"]*['"]+/i) {|result| 
-        line = result[0]; 
+        line = result[0];
         line.gsub!(/^.*?url\(/,''); 
         line.gsub!(/\)/,''); 
         images.push Image.new(page_host, URI.escape(line))
       }
 
       images.each {|image|
-        url = URI.parse(image.full_src)
+        url = URI.parse(URI.encode(image.full_src.strip))
         request = Net::HTTP::Get.new(url.path)
 
         Net::HTTP.start(url.host) {|http|
           file = open("#{dir_path+image.file_name}", "wb")
           begin
             http.request_get(request.path, "User-Agent"=> USER_AGENT) do |response|
-              response.read_body do |segment|
-                file.write(segment)
+              file_size = response['content-length']
+              if file_size.to_i > MIN_SIZE
+                response.read_body do |segment|
+                  file.write(segment)
+                end
+              else
+                File.delete(file)
               end
             end
           ensure
--- lib/image_hopper/image.rb
+++ lib/image_hopper/image.rb
@@ -2,13 +2,12 @@ module ImageHopper
   class Image
     attr_accessor :src, :file_name, :page_host, :full_src
   
-    MIN_SIZE = 100
     IMAGE_EXTENSIONS = ["jpg","jpeg","png","gif","ico","svg","bmp"]
   
     def initialize(page_host, src)
       @page_host = page_host
       @src = src
-      @file_name = @src.sub(/.*\//, '')
+      @file_name = @src.sub(/.*\//, '').gsub(">", '')
       @full_src = ((@src =~ /http/) ? @src : ('http://' + @page_host + '/' +  @src.sub(/^\/+/,'')))
     end
   end
